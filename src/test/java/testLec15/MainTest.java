f6f85cfaac6903a4789014280154662776e25f32
KDrops <knihadrops@gmail.com>
2024-03-26 00:35:03 +0100

    new first change

---
+++ src/test/java/testLec15/MainTest.java
@@ -0,0 +1,25 @@
+package testLec15;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainTest {
+    public void testGoogle(){
+        //vcvbnm,mbnvbcbnm,m,
+        //hgfcx b nm,m,kjhngvbcfbnm
+
+
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+        WebElement text = driver.findElement(By.id("gNO89b"));
+        text.sendKeys("Selenium");
+
+        WebElement button = driver.findElement(By.className("gNO89b"));
+        button.click();
+
+    }
+}
